92926697bf6db6eac2f8ca4e7fe78e0fef79de67
unknown <boedi15@gmail.com>
2021-08-16 05:39:43 -0600

    first two questions

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 // Require
 const inquirer = require('inquirer');
 
+//questions
 const promptUser = () => {
     return inquirer.prompt([
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name? (Required)',
+        message: 'What is your name of your project? (Required)',
         validate: nameInput => {
           if (nameInput) {
             return true;
           } else {
-            console.log('Please enter your name!');
+            console.log('Please enter name of project to continue!');
             return false;
           }
         }
       },
-     
+     //Project Description
+     {
+         type: 'input',
+         name: 'description',
+         message: 'Provide a description of the project. (Required)',
+         validate: desciptionInput => {
+             if (descriptionInput) {
+                 return true;
+             } else {
+                 console.log('You need to provide a description!');
+                 return false;
+             }
+         }
+     },
