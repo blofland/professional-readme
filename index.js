06fff0ea77b3f22b44bdb0a1f59567fc73c34ab5
unknown <boedi15@gmail.com>
2021-08-16 02:28:16 -0600

    added package

---
+++ index.js
@@ -0,0 +1,19 @@
+// Require
+const inquirer = require('inquirer');
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
+      },
+     
